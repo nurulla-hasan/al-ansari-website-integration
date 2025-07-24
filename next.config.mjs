908d76c5a74e4ec4c1a4e24df88b22fbbb36d736
Nurulla Hasan <85323050+nurulla-hasan@users.noreply.github.com>
2025-07-24 11:35:29 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,36 +1,19 @@
-
-import createNextIntlPlugin from 'next-intl/plugin';
+import createNextIntlPlugin from 'next-intl/plugin'; 
 
 const withNextIntl = createNextIntlPlugin();
-const parsedBaseUrl = new URL("http://13.62.48.83:5001" || 'http://localhost:3000');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    productionBrowserSourceMaps: true,
-    images: {
-        remotePatterns: [
-            {
-                protocol: parsedBaseUrl.protocol.replace(':', ''), 
-                hostname: parsedBaseUrl.hostname,
-                port: parsedBaseUrl.port || '', 
-                pathname: '/**',
-            },
-            {
-              protocol: 'https',
-              hostname: parsedBaseUrl.hostname,
-              port: parsedBaseUrl.port || '',
-              pathname: '/**',
-            }
-            // {
-            //     protocol: "http", 
-            //     hostname: "**",
-            // },
-            // {
-            //     protocol: "https", 
-            //     hostname: "**",
-            // },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '13.62.48.83', 
+        port: '5001',         
+        pathname: '/**', 
+      },
+    ],
+  },
 };
 
 export default withNextIntl(nextConfig);
